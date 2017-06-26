e52194128bd07ae950265c7e217749970be4da33
anjanaNewput <anjana@newput.com>
2017-06-26 11:37:00 +0530

    Initial project setup

---
+++ app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+ReactDOM.render(<h1>Hello React</h1>, document.getElementById('root'));
